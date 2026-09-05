1feee0cde459218fbad4a073e4fad792dda68b63
ykz89 <ying@zhan.co.nl>
2026-09-05 19:26:21 +0200

    Remove le_chiffre

--- keyboards/sporkus/le_chiffre_stm32/keymaps/vial/rules.mk
+++
@@ -1,9 +0,0 @@
-# The keymap is a thin adapter over users/ykz89; USER_NAME points the classic
-# userspace lookup there since this keymap is named "vial", not "ykz89".
-USER_NAME = ykz89
-
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-VIALRGB_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-COMBO_ENABLE = yes
